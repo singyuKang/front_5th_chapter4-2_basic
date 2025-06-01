4f0d4f7cf6af7f8ee9fe97ca9cd42e6b18738abc
singyuKang <rkdtlscks123@naver.com>
2025-06-02 01:34:32 +0900

    test : git action 테스트

--- js/products.js
+++ js/products.js
@@ -1,7 +1,6 @@
 async function loadProducts() {
   const response = await fetch("https://fakestoreapi.com/products");
   const products = await response.json();
-  console.log("🚀 ~ loadProducts ~ products:", products);
   displayProducts(products);
 }
 
